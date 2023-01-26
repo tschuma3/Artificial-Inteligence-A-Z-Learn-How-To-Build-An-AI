1507ac4adebe45e097ac49d816c6f10f369505b9
Tyler Schumacher <tschuma3@uccs.edu>
2023-01-26 13:37:57 -0700

    Added comments to better understand the code

--- Self Driving Car Using Deep Q-Learning and ANN/ai.py
+++ Self Driving Car Using Deep Q-Learning and ANN/ai.py
@@ -52,47 +52,65 @@ class Dqn():
         self.last_action = 0
         self.last_reward = 0
 
+    #Takes an action based on the state given
     def select_action(self, state):
         probs = F.softmax(self.model(Variable(state, volatile=True)) * 100) #T=100
         action = probs.multinomial()
         return action.data[0, 0]
 
+    #Implements the Deep Q Learning equation 
     def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
         outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
         next_output = self.model(batch_next_state).detach().max(1)[0]
-        target = self.gamma * next_output + batch_reward
-        td_loss = F.smooth_l1_loss(outputs, target)
-        self.optimizer.zero_grad()
-        td_loss.backward(retain_variables=True)
+        target = self.gamma * next_output + batch_reward #gamma * Q + R
+        td_loss = F.smooth_l1_loss(outputs, target) #Uses the Hubber Loss
+        self.optimizer.zero_grad() #Stochastic Gradient Descent
+        td_loss.backward(retain_variables=True) #Backward Propagation 
         self.optimizer.step()
 
+    #Updates the data
     def update(self, reward, new_signal):
-        new_state = torch.Tensor(new_signal).float().unsqueeze
+        #Gets a new state
+        new_state = torch.Tensor(new_signal).float().unsqueeze(0)
+        #Updates the memory, keeping everything as a Tensor
         self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
+        #Takes an action
         action = self.select_action(new_state)
-        if len(self.memory.memory) > 100:
+        #Lets the model learn from 100 states, next states, rewards, and action
+        if len(self.memory.memory) > 100: #self.memory.mermory == self . memory(of the Dnq class) . memory(of the ReplayMemory class)
             batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(100)
-            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
+            self.learn(batch_state, batch_next_state, batch_reward, batch_action) 
+        #Updates the last action, last state, last reward, and appends the reward to the reward_window list
         self.last_action = action
         self.last_state = new_state
         self.last_reward = reward
         self.reward_window.append(reward)
+        #Deletes the first reward if it is over 1000 elements
         if len(self.reward_window) > 1000:
             del self.reward_window[0]
+        #Returns the action
         return action
 
+    #Gives a score based on the reward window
     def score(self):
-        return sum(self.reward_window) / (len(self.reward_window) + 1.) 
+        #Calculates the mean
+        return sum(self.reward_window) / (len(self.reward_window) + 1.) #The '+ 1.' allows for the system to never be 0, therefor keeping it from crashing
 
+    #Saves the current model
     def save(self):
+        #Saves the last versions of the weights and optimizer
+        #Saved in a dictionary
         torch.save({'state_dict': self.model.state_dict(),
                     'optimizer': self.optimizer.state_dict()},
                      'last_brain.pth')
 
+    #Loads the last saved model
     def load(self):
+        #Makes sure the 'last_brain.pth' exists
         if os.path.isfile(r'last_brain.pth'):
             print("=> loading chechpoint... ")
             checkpoint = torch.load(r'last_brain_pth')
+            #Updates the model parameters and the optimizer based on the 'last_brain.pth'
             self.model.load_state_dict(checkpoint['state_dict'])
             self.optimizer.load_state_dict(checkpoint['optimizer'])
             print("Done!")
